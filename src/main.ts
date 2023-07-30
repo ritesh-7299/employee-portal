bbf0744567550ffc0d8bafcbc003af53f5ac82a7
Ritesh Macwan <riteshmacwan07@gmail.com>
2023-07-30 19:31:56 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //For enabling cors
-  app.enableCors();
+  //app.enableCors();
 
+  app.enableCors(
+    { 
+      origin: ['https://employee-portal-rd.vercel.app'],
+      methods: ['POST', 'PUT', 'DELETE', 'GET']
+    }
+  );
+  
   //For enabling validation errors globally
   app.useGlobalPipes(new ValidationPipe());
 
